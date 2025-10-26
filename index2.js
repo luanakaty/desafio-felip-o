19aab5b73582c72bf707e28f9f2380769cb186e8
luana <luanakatybs@gmail.com>
2025-10-26 17:51:56 -0300

    Desenvolvi a função calcularNivel para a entrega do projeto

---
+++ index2.js
@@ -0,0 +1,23 @@
+function calcularNivel(vitorias, derrotas) {
+    let saldo = vitorias - derrotas;
+    let nivel = "";
+
+    if (saldo <= 10) {
+        nivel = "Ferro";
+    } else if (saldo >= 11 && saldo <= 20) { 
+        nivel = "Bronze";
+    } else if (saldo >= 21 && saldo <= 50) {
+        nivel = "Prata";
+    } else if (saldo >= 51 && saldo <= 80) {
+        nivel = "Ouro";
+    } else if (saldo >= 81 && saldo <= 90) {
+        nivel = "Diamante";
+    } else if (saldo >= 91 && saldo <= 100){
+        nivel = "Lendário";
+    } else {
+       nivel = "Imortal";
+    }
+
+    console.log(`O Herói tem de saldo ${saldo} e está no nivel ${nivel}`);
+}
+ 
